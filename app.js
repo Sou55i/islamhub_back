d43b85cbc3c8514560863ab62c896120103773d6
NexGuardX <karimromdhane7@gmail.com>
2025-02-14 22:43:09 +0100

    add app.js and www.js

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+//import router from "./app/routes/index.router.js";
+//import { errorsHandler } from "./app/middlewares/index.middleware.js";
+import logger from "morgan";
+import fs from "fs";
+//import error404 from "./app/middlewares/error404.js";
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use(logger("dev"));
+app.use(
+  logger("common", {
+    stream: fs.createWriteStream("./logs/access.log", { flags: "a" }),
+  })
+);
+
+//app.use(router);
+//app.use(errorsHandler);
+//app.all("*", error404);
+export default app;
